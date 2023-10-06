f2485ea315b78c785f7cbf82006d8922e10d6607
Aline Tavares <alaisgomes@gmail.com>
2023-10-06 11:24:14 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "cafetopia-dev-109994",
-  url: "https://cafetopia-dev-109994.botics.co",
-  api: "https://cafetopia-dev-109994.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "cafetopia-dev-109994",
+  "url": "https://cafetopia-dev-109994.botics.co",
+  "api": "https://cafetopia-dev-109994.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
